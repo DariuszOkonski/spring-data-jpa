5624ef1343a610f377426437d078f97a786f405c
Dariusz Okonski <darek200180@gmail.com>
2022-01-05 20:35:41 +0000

    exploring repository methods

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class Application {
 
@@ -22,8 +24,46 @@ public class Application {
                     "maria.jones@amigoscode.edu",
                     21);
 
+            Student ahmed = new Student(
+                    "Ahmed",
+                    "Ali",
+                    "ahmed.ali@amigoscode.edu",
+                    21
+            );
 
-            studentRepository.save(maria);
+            System.out.println("==> Adding maria and ahmed");
+            studentRepository.saveAll(List.of(maria, ahmed));
+
+            System.out.println("===================================");
+
+            System.out.println("==> Number of students");
+            System.out.println(studentRepository.count());
+
+            studentRepository.findById(2L)
+                    .ifPresentOrElse(
+                            System.out::println,
+                            () -> {
+                                System.out.println("Student with id 2 not found");
+                            });
+
+
+            studentRepository.findById(3L)
+                    .ifPresentOrElse(
+                            System.out::println,
+                            () -> {
+                                System.out.println("Student with id 3 not found");
+                            }
+                    );
+
+            System.out.println("==> Select all students");
+            List<Student> students = studentRepository.findAll();
+            students.forEach(System.out::println);
+
+            System.out.println("==> Delete maria");
+            studentRepository.deleteById(1L);
+
+            System.out.println("==> Number of students");
+            System.out.println(studentRepository.count());
         };
     }
 
